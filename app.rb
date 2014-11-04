ffa96e179811428f068c590c42dd0874ebff37ea
Pablo Oldani <oldani.pablo@gmail.com>
2014-11-04 16:42:54 -0300

    Add swagger info.

--- app.rb
+++ app.rb
@@ -5,4 +5,18 @@ class SAITW < Grape::API
   format :json
 
   mount SAITW::Task
+
+  add_swagger_documentation({
+    api_version: 'v1',
+    hide_format: true,
+    hide_documentation_path: true,
+    info: {
+      title: 'My Todo, The Smalest API in the world',
+      description: 'A application to keep track of yout todo list',
+      contact: '',
+      license: 'GPL v3',
+      license_url: 'todo',
+      terms_of_service_url: 'todo'
+    }
+  })
 end
