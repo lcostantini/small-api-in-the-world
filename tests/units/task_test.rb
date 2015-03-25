4804e2a67ea4e47dca8b9cc4e69a6294a11c88c8
lcostantini <costantinileandro1@gmail.com>
2015-03-25 12:41:24 -0300

    Testing new functionalities.

--- tests/units/task_test.rb
+++ tests/units/task_test.rb
@@ -1,27 +1,43 @@
 require './tests/test_helper'
 
 scope do
-
   setup do
-    @task = Task.create name: 'Learn Angular'
+    @task = Task.create name: 'Test api', category: 'testing'
   end
 
-  test 'save task' do
-    assert Task.all.count == 1
+  test 'Task is saved' do
+    assert_equal Task.all.count, 1
   end
 
-  test 'a task must be on "todo" state when is created' do
-    assert @task.state == 'todo'
+  test 'A task must be on "todo" state when is created' do
+    assert_equal @task.state, 'todo'
   end
 
   test '#todos should show all the task whit todo state' do
-    assert Task.todos.count == 1
+    assert_equal Task.todos.count, 1
   end
 
   test '#done! should set a task as done' do
     @task.done!
-    assert @task.state == 'done'
-    assert Task.todos.count == 0
+    assert_equal @task.state, 'done'
+    assert_equal Task.todos.count, 0
   end
 
+  test '#all should show all the task' do
+    assert_equal Task.all.count, 1
+  end
+
+  test '#categories should return all the task with some category' do
+    assert_equal Task.categories('testing').count, 1
+  end
+
+  test 'update a task' do
+    @task.update category: 'update'
+    assert_equal @task.category, 'update'
+  end
+
+  test 'delete a task' do
+    @task.delete
+    assert_equal Task.all.count, 0
+  end
 end
