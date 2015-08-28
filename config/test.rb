f5b7ee652f0805a3e964ce443d7d7ba463424909
lcostantini <costantinileandro1@gmail.com>
2015-08-28 12:05:34 -0300

    Move require to config test.

--- config/test.rb
+++ config/test.rb
@@ -2,3 +2,13 @@ require 'cutest'
 require 'pry'
 
 ENV['REDISTOGO_URL'] = "redis://localhost:6379/2"
+
+require 'rack/test'
+
+class Cutest::Scope
+  include Rack::Test::Methods
+
+  def app
+    Cuba
+  end
+end
--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -1,12 +1,5 @@
 ENV['RACK_ENV'] = 'test'
 
 require './app'
-require 'rack/test'
 
-class Cutest::Scope
-  include Rack::Test::Methods
-
-  def app
-    Cuba
-  end
 end
