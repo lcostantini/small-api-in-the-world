1ef3b57ad0b35bbf3fffeb8c9d67ba95ff26512b
lcostantini <costantinileandro1@gmail.com>
2015-09-01 12:02:19 -0300

    Move authentication to app.rb
    Clean api/tasks by moving the method authenticate!.
    Remove duplicated validations for method authenticate!.

--- api/tasks.rb
+++ api/tasks.rb
@@ -2,10 +2,6 @@ def current_user
   @current_user ||= User.find_or_create env['HTTP_USER_TOKEN']
 end
 
-def authenticate!
-  raise(StandardError, 'Unauthorized') unless env['HTTP_USER_TOKEN'] && current_user
-end
-
 def merge_id_in_attr list
   list.to_a.map { |t| t.attributes.merge id: t.id }
 end
@@ -16,62 +12,50 @@ end
 
 class Tasks < Cuba
   define do
-    begin
-
-      authenticate!
-
-      on get do
-        on root do
-          tasks = merge_id_in_attr current_user.tasks.find state: 'todo'
-          res.write tasks.to_json
-        end
-
-        on 'all' do
-          tasks = merge_id_in_attr current_user.tasks
-          res.write tasks.to_json
-        end
-
-        on 'category', param('topic') do |query|
-          res.write merge_id_in_attr(current_user.tasks.find(category: query)).to_json
-        end
+    on get do
+      on root do
+        tasks = merge_id_in_attr current_user.tasks.find state: 'todo'
+        res.write tasks.to_json
       end
 
-      on post do
-        task = current_user.tasks.add Task.create json_body[:task]
-        res.write task
-        res.status = 201
+      on 'all' do
+        tasks = merge_id_in_attr current_user.tasks
+        res.write tasks.to_json
       end
 
-      on ':id' do |id|
-        task = Task[id]
-
-        on put do
-          on root do
-            task = task.update json_body[:task].reject { |_, v| v.nil? }
-            res.write task.attributes.to_json
-          end
-
-          on 'done' do
-            res.write task.done!
-          end
-
-          on 'undone' do
-            res.write task.undone!
-          end
-        end
-
-        on delete do
-          current_user.tasks.delete task
-          res.status = 200
-        end
-      end
-
-    rescue StandardError => e
-      on true do
-        res.status = 401
-        res.write errors: e.message
+      on 'category', param('topic') do |query|
+        res.write merge_id_in_attr(current_user.tasks.find(category: query)).to_json
       end
     end
 
+    on post do
+      task = current_user.tasks.add Task.create json_body[:task]
+      res.write task
+      res.status = 201
+    end
+
+    on ':id' do |id|
+      task = Task[id]
+
+      on put do
+        on root do
+          task = task.update json_body[:task].reject { |_, v| v.nil? }
+          res.write task.attributes.to_json
+        end
+
+        on 'done' do
+          res.write task.done!
+        end
+
+        on 'undone' do
+          res.write task.undone!
+        end
+      end
+
+      on delete do
+        current_user.tasks.delete task
+        res.status = 200
+      end
+    end
   end
 end
--- app.rb
+++ app.rb
@@ -1,7 +1,26 @@
 require './config/application'
 
+def authenticate!
+  raise 'Unauthorized' unless current_user
+end
+
+def current_user
+  User.find_or_create env['HTTP_USER_TOKEN']
+end
+
 Cuba.define do
-  on 'tasks' do
-    run Tasks
+  begin
+
+    authenticate!
+
+    on 'tasks' do
+      run Tasks
+    end
+
+  rescue StandardError => e
+    on true do
+      res.status = 401
+      res.write errors: e.message
+    end
   end
 end
