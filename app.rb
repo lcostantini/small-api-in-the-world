3e75a4cd618872e1d4056cfd8c9f090fd84c8d03
lcostantini <leandro.costantini@tele2.com>
2019-05-27 14:04:31 +0200

    Add environment file to handle requires and config

--- app.rb
+++ app.rb
@@ -1,23 +1,5 @@
-require 'cuba'
-require 'ohm'
-require 'json'
-require './api/tasks'
-Dir["./models/*.rb"].each { |rb| require rb  }
-
-Ohm.redis = Redic.new(ENV['REDISCLOUD_URL'] || "redis://localhost:6379")
-
-def validate_access!
-  raise(StandardError, 'The file doesn\'t contain the token') if env['HTTP_USER_TOKEN'].empty?
-  raise(StandardError, 'Unauthorized') unless current_user
-end
-
-def current_user
-  @current_user ||= User.find_or_create env['HTTP_USER_TOKEN']
-end
-
 Cuba.define do
   begin
-
     validate_access!
 
     on 'tasks' do
